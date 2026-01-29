d0d82d02e149b72f9ed76e622d24e9ea17c41f87
Anuj Sharma <anujsharmajan06@gmail.com>
2026-01-29 22:30:17 +0530

    Initial Spring Boot project structure

---
+++ src/main/java/com/community/safetyalert/SafetyalertApplication.java
@@ -0,0 +1,13 @@
+package com.community.safetyalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyalertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/community/safetyalert/SafetyalertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.safetyalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyalertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

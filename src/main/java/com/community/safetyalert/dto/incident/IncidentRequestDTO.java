ba5e917fcf38af38033f4f98bea35112bfc206bd
Anuj Sharma <anujsharmajan06@gmail.com>
2026-02-07 23:15:58 +0530

    Add validation annotations to request DTOs

--- src/main/java/com/community/safetyalert/dto/incident/IncidentRequestDTO.java
+++ src/main/java/com/community/safetyalert/dto/incident/IncidentRequestDTO.java
@@ -1,16 +1,31 @@
 package com.community.safetyalert.dto.incident;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class IncidentRequestDTO {
+
+    @NotBlank(message = "Title is required")
     private String title;
+
+    @NotBlank(message = "Description is required")
     private String description;
+
+    @NotBlank(message = "Category is required")
     private String category;
+
+    @NotNull(message = "Latitude is required")
     private Double latitude;
+
+    @NotNull(message = "Longitude is required")
     private Double longitude;
+
+    @NotBlank(message = "Location name is required")
     private String locationName;
+
     private String mediaUrl;
 }
--- src/main/java/com/community/safetyalert/dto/user/UserRequestDTO.java
+++ src/main/java/com/community/safetyalert/dto/user/UserRequestDTO.java
@@ -1,12 +1,22 @@
 package com.community.safetyalert.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserRequestDTO {
+
+    @NotBlank(message = "Name cannot be empty")
     private String name;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
 }

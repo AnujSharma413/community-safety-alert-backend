97846eb7026b92c4d6a9e9c47571b93b5676729c
Anuj Sharma <anujsharmajan06@gmail.com>
2026-02-11 22:47:08 +0530

    Add UserService implementation with DTO-based user registration

---
+++ src/main/java/com/community/safetyalert/service/impl/UserServiceImpl.java
@@ -0,0 +1,68 @@
+package com.community.safetyalert.service.impl;
+
+import com.community.safetyalert.dto.user.UserRequestDTO;
+import com.community.safetyalert.dto.user.UserResponseDTO;
+import com.community.safetyalert.model.Role;
+import com.community.safetyalert.model.User;
+import com.community.safetyalert.repository.UserRepo;
+import com.community.safetyalert.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepo userRepo;
+
+    public UserServiceImpl(UserRepo userRepo){
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserResponseDTO registerUser(UserRequestDTO dto) {
+
+        User user = new User();
+        user.setName(dto.getName());
+        user.setEmail(dto.getEmail());
+        user.setPassword(dto.getPassword());
+        user.setRole(Role.USER);
+
+        User savedUser = userRepo.save(user);
+
+        return new UserResponseDTO(
+                savedUser.getId(),
+                savedUser.getName(),
+                savedUser.getEmail(),
+                savedUser.getRole().name()
+        );
+    }
+
+    @Override
+    public UserResponseDTO getUserById(Long id) {
+
+        User user = userRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return new UserResponseDTO(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getRole().name()
+        );
+    }
+
+    @Override
+    public List<UserResponseDTO> getAllUsers() {
+
+        return userRepo.findAll()
+                .stream()
+                .map(user -> new UserResponseDTO(
+                        user.getId(),
+                        user.getName(),
+                        user.getEmail(),
+                        user.getRole().name()
+                ))
+                .toList();
+    }
+}
